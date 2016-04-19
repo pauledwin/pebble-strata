234dedb9692f3c338c02af40660c7535442245f5
Paul Edwin <paul.edwin@deguello.org>
2016-04-20 00:10:03 +0100

    Split out credentials and start tidying file

---
+++ redirect/credentials-secret.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = Object.freeze({
+    // define your OAuth-application credentials
+    STRATA_CONSUMER_KEY: 'xxxxxxxxxxxxxxxxxxxx',
+    STRATA_CONSUMER_SECRET: 'xxxxxxxxxxxxxxxxxxxx'
+});
--- redirect/index.js
+++ redirect/index.js
@@ -1,62 +1,71 @@
-// define libraries you would need
-var oauth = require('oauth')
-var AWS = require('aws-sdk')
-var cuid = require('cuid')
+'use strict';
 
-// define your OAuth-application credentials
-var twitterConsumerKey = 'xxxxxxxxxxxxxxxxxxxx'
-var twitterConsumerSecret = 'xxxxxxxxxxxxxxxxxxxx'
+// define libraries you would need
+var oauth = require('oauth');
+var AWS = require('aws-sdk');
+var cuid = require('cuid');
+
+var credentials = require('credentials-hidden');
 
 // ensure AWS is requesting the nearest region
 AWS.config.update({
-  region: 'eu-west-1'
-})
+    region: 'eu-west-1'
+});
 
 // the function that will be executed by Lambda
 exports.handler = function (event, context) {
-  // define unique token to identify callback data for which user it is ment
-  var idToken = cuid()
+    // define unique token to identify callback data for which user it is ment
+    var idToken = cuid()
 
-  // define OAuth signature
-  var api = new oauth.OAuth(
-    'https://twitter.com/oauth/request_token',
-    'https://twitter.com/oauth/access_token',
-    twitterConsumerKey,
-    twitterConsumerSecret,
-    '1.0A',
-    'https://api.yourdomain.com/connect/callback?token=' + idToken,
-    'HMAC-SHA1'
-  )
+    // define OAuth signature
+    // var api = new oauth.OAuth(
+    //   'https://twitter.com/oauth/request_token',
+    //   'https://twitter.com/oauth/access_token',
+    //   twitterConsumerKey,
+    //   twitterConsumerSecret,
+    //   '1.0A',
+    //   'https://api.yourdomain.com/connect/callback?token=' + idToken,
+    //   'HMAC-SHA1'
+    // )
+    var api = new oauth.OAuth(
+        'https://www.strava.com/oauth/authorize',
+        'https://www.strava.com/oauth/token',
+        credentials.STRATA_CONSUMER_KEY,
+        credentials.STRATA_CONSUMER_SECRET,
+        '1.0A',
+        'https://api.yourdomain.com/connect/callback?token=' + idToken,
+        'HMAC-SHA1'
+    );
 
-  // get request token and redirect users to that URL
-  api.getOAuthRequestToken(function (error, oauthToken, oauthTokenSecret, result) {
-    if (error) {
-      context.fail(error)
-    } else {
-      // store tokens to be able to map the data correctly
-      var db = new DBConnector() // this can be DynamoDB, Firebase, etc..
-      var params = {
-        table: 'feed-users',
-        item: {
-          id: oauthToken,
-          secret: oauthTokenSecret,
-          idToken: idToken
-        }
-      }
-
-      // save tokens to use with the callback
-      db.put(params, function (err, data) {
-        if (err) {
-          console.error('Unable to add item. Error JSON:', JSON.stringify(err, null, 2))
-          context.fail(err)
+    // get request token and redirect users to that URL
+    api.getOAuthRequestToken(function (error, oauthToken, oauthTokenSecret, result) {
+        if (error) {
+            context.fail(error)
         } else {
-          console.log('Added item:', JSON.stringify(data, null, 2), token)
-          // redirect user to correct URL to acknowledge the new share settings
-          context.succeed({
-            location: 'https://twitter.com/oauth/authorize?oauth_token=' + oauthToken
-          })
+            // store tokens to be able to map the data correctly
+            var db = new DBConnector() // this can be DynamoDB, Firebase, etc..
+            var params = {
+                table: 'feed-users',
+                item: {
+                    id: oauthToken,
+                    secret: oauthTokenSecret,
+                    idToken: idToken
+                }
+            }
+
+            // save tokens to use with the callback
+            db.put(params, function (err, data) {
+                if (err) {
+                    console.error('Unable to add item. Error JSON:', JSON.stringify(err, null, 2))
+                    context.fail(err)
+                } else {
+                    console.log('Added item:', JSON.stringify(data, null, 2), token)
+                    // redirect user to correct URL to acknowledge the new share settings
+                    context.succeed({
+                        location: 'https://twitter.com/oauth/authorize?oauth_token=' + oauthToken
+                    })
+                }
+            })
         }
-      })
-    }
-  })
+    })
 }
