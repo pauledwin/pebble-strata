79014b05ed63fc30f3eb3e3605be3375834cabbc
Paul Edwin <paul.edwin@deguello.org>
2016-04-22 00:50:12 +0100

    Add config page for Strava authentication.

--- common/credentials-secret.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = Object.freeze({
-    // define your OAuth-application credentials
-    STRATA_CONSUMER_KEY: 'xxxxxxxxxxxxxxxxxxxx',
-    STRATA_CONSUMER_SECRET: 'xxxxxxxxxxxxxxxxxxxx'
-});
